ee3bc2f3ee4cfbce8cc953dc3ff756c445df01f5
Zabi <zbvmihai@gmail.com>
2023-03-15 21:46:25 +0000

    Add refresh button and its functionality

--- app/src/main/java/com/zabava/weatherapp/MainActivity.kt
+++ app/src/main/java/com/zabava/weatherapp/MainActivity.kt
@@ -16,6 +16,8 @@ import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.google.android.gms.location.*
@@ -47,8 +49,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         if (!isLocationEnabled()) {
             Toast.makeText(this, "Please turn on your location!", Toast.LENGTH_SHORT).show()
@@ -268,6 +268,22 @@ class MainActivity : AppCompatActivity() {
         val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
         sdf.timeZone = TimeZone.getDefault()
         return sdf.format(date)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.actionRefresh -> {
+                requestLocationData()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
 
     }
 }
